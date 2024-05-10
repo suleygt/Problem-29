616437f905f7ed56b347d6a0a07e077741db94fe
suleygt <suleyigit5454@icloud.com>
2024-05-10 19:08:22 +0300

    innitinal commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { EnvelopeIcon } from '@heroicons/react/20/solid'
+import { useReducer } from 'react'
+import { useState }  from 'react';
+
+export default function InviteUsers() {
+  const [email, setEmail] = useState('');
+  const [inviteSent, setInviteSent] = useState(false);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Here you can implement your logic for sending the invitation
+    // For now, let's just simulate the invitation sending process
+    setTimeout(() => {
+      setInviteSent(true);
+      setEmail('');
+    }, 1000);
+  };
+
+  return (
+    <div className='mx-auto p-8 max-w-lg'>
+      <div>
+        <Header />
+        <form className='mt-6 flex' onSubmit={handleFormSubmit}>
+          <label htmlFor='email' className='sr-only'>
+            E-mail adresiniz
+          </label>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            className='px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            placeholder='E-posta giriniz'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button
+            type='submit'
+            className='ml-4 flex-shrink-0 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+          >
+            Davetiye gönderin
+          </button>
+        </form>
+      </div>
+      {inviteSent && (
+        <div className='mt-10'>
+          <h3 className='text-sm font-medium text-gray-500'>
+            Ekip üyesi <span className='text-indigo-500'>{email}</span> eklendi!
+          </h3>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className='text-center'>
+      <EnvelopeIcon className='mx-auto h-12 w-12 text-gray-400' />
+      <h2 className='mt-2 text-base font-semibold leading-6 text-gray-900'>
+        Ekip üyelerini davet edin
+      </h2>
+      <p className='mt-1 text-sm text-gray-500'>
+        Projenize henüz herhangi bir ekip üyesi eklemediniz. Projenin sahibi
+        olarak, ekip üyesi izinlerini yönetebilirsiniz.
+      </p>
+    </div>
+  );
+}
